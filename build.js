37cb0c49a43c91b5eb3456cb15ee5c9c9381eb41
hackpro4005 <114504302+hackpro4005@users.noreply.github.com>
2024-04-12 16:32:02 +0300

    Add files via upload

---
+++ build.js
@@ -0,0 +1,11 @@
+{
+  "name"; "Chill out";
+  "version"; "0.0.1";
+  "description"; "Take a chill pill";
+  "source"; [
+    "theme/Chill out.json",
+    
+  ];
+  "output"; "./dist/Chill out.vsix";
+  "license"; "MIT";
+}
